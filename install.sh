80f865af90ef3a485d3771e39ae01f4bf0063e64
Nat Zimmermann <nat@natzim.me>
2015-08-19 16:48:06 +0100

    Create scripts dir on install

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ old_dir=$home/.dotfiles_old
 files="vimrc xinitrc Xresources zshrc zprofile zlogin config/bspwm/bspwmrc config/sxhkd/sxhkdrc scripts/panel scripts/panel_bar"
 
 mkdir -p $old_dir
+mkdir -p $home/.scripts
 
 for file in $files; do
     mv $home/.$file $old_dir
